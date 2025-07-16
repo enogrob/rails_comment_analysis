33ad425f2f9054ee835abea01d0044ee4ece859c
enogrob <enogrob@gmail.com>
2025-07-16 00:34:52 -0300

    add-caching-users-change

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
   has_many :posts
+
+  after_commit :invalidate_metrics_cache
+
+  private
+
+  def invalidate_metrics_cache
+    $redis.del("user_metrics:#{self.id}")
+    $redis.del("group_metrics")
+  end
 end
