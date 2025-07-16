51aaec4f456e2b56d30ec6e693c4c0cb8bfab6c0
enogrob <enogrob@gmail.com>
2025-07-16 00:48:39 -0300

    add-reprocessing-on-keyword-change

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -1,10 +1,16 @@
 class Keyword < ApplicationRecord
-  after_commit :invalidate_metrics_cache
+  after_commit :invalidate_metrics_cache_and_reprocess_comments
 
   private
 
-  def invalidate_metrics_cache
+  def invalidate_metrics_cache_and_reprocess_comments
+    # Invalidate metrics cache
     $redis.del("group_metrics")
     User.find_each { |user| $redis.del("user_metrics:#{user.id}") }
+
+    # Reprocess all comments for approval
+    Comment.find_each do |comment|
+      CommentApprovalService.approve_or_reject(comment)
+    end
   end
 end
